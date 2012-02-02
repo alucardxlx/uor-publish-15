e45fdbb3dd2c847b3c941e38eaa69873d4662b14
malbolger@gmail.com <malbolger@gmail.com@96c2f633-49b2-c093-6fe6-0b779d882d05>
2012-02-02 03:55:17 +0000

    restricted PublicMoongate.cs to only allow Felucca choices.

--- trunk/Server/Scripts/Items/Misc/PublicMoongate.cs
+++ trunk/Server/Scripts/Items/Misc/PublicMoongate.cs
@@ -284,8 +284,8 @@ namespace Server.Items
 					new PMEntry( new Point3D(  270,  628, 15 ), 1063414 )  // Homare-Jima
 				} );
 
-		public static readonly PMList[] UORLists		= new PMList[] { Trammel, Felucca };
-		public static readonly PMList[] UORListsYoung	= new PMList[] { Trammel };
+		public static readonly PMList[] UORLists		= new PMList[] { Felucca };
+		public static readonly PMList[] UORListsYoung	= new PMList[] { Felucca };
 		public static readonly PMList[] LBRLists		= new PMList[] { Trammel, Felucca, Ilshenar };
 		public static readonly PMList[] LBRListsYoung	= new PMList[] { Trammel, Ilshenar };
 		public static readonly PMList[] AOSLists		= new PMList[] { Trammel, Felucca, Ilshenar, Malas };
