59e7eb787cccd188d6272f49bf4547f1f4c268df
malbolger@gmail.com <malbolger@gmail.com@96c2f633-49b2-c093-6fe6-0b779d882d05>
2012-02-16 10:06:03 +0000

    ** Added MemoryEditor.dll for C#
    ** Added MemoryAPI.cs
    
    ** Completed Functionality to read the value of memory Addresses (life/mana/stam/gold...ect ect ect)
    
    ** Have Found the Memory Adress for life in DAoC with a simple if statement when life drops below 20% (can be instant heal)

--- Project Files/DAoC Hook/DAoC Hook/Form1.Designer.cs
+++ Project Files/DAoC Hook/DAoC Hook/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -37,15 +39,37 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 36);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Send Key On Timer";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Do If Under 20% Life";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 116);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 264);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +80,8 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Project Files/DAoC Hook/DAoC Hook/Form1.cs
+++ Project Files/DAoC Hook/DAoC Hook/Form1.cs
@@ -9,12 +9,43 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Threading;
+using MemoryEditor;
+using System.Globalization;
+
 
 namespace WindowsFormsApplication1
 {
+    
+
     public partial class Form1 : Form
     {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(UInt32 dwDesiredAccess, Int32 bInheritHandle, UInt32 dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesWritten);
+
+
+
         Process[] processes = Process.GetProcessesByName("game.dll");  //looks for in running processes.
+        Memory oMemory = new Memory(); //For memory read/write
+        Memory value = new Memory();
+
+        // DAoC Life Pointer? 0145CDF8
+
+//               [DllImport("Kernel32.Dll")] 
+//public static extern IntPtr OpenProcess(WindowHandle dwDesiredAccess, bool bInheritance, DWORD dwProcessId); 
+
+//[DllImport("Kernel32.Dll")] 
+//public static extern bool ReadProcessMemory(IntPtr hProcess,IntPtr BaseAddress, out byte[] buffer, Int32 b_size, out Int32 dwBytesRead); 
+
+
 
 
  
@@ -45,8 +76,87 @@ namespace WindowsFormsApplication1
 
         }
 
-      
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (processes.Length != 0) //If the process exists 
+            {
+                IntPtr WindowHandle = processes[0].MainWindowHandle;
+                WindowsAPI.SwitchWindow(WindowHandle);
+                Thread.Sleep(1000); //wait while window is switched
+              
+                int LifeAddr = Addr.ToDec("0145CDF8");
+                oMemory.ReadProcess = processes[0]; //Sets the Process to Read/Write From/To 
+                oMemory.Open(); //Open Process 
+
+
+
+               
+                
+
+          
+               // int Life = bytesRead;
+                int PanicPercent = 20;
+
+                int loop = 15;
+
+              
+
+                while (loop > 1)
+                {
+                    int bytesReaded; //This will hold the number of bytes that were successfully read 
+                    byte[] ThisIs4Bytes = oMemory.Read((IntPtr)LifeAddr, 4, out  bytesReaded); //Reads 4 bytes to a byte array 
+                    int ReadValue = BitConverter.ToInt32(ThisIs4Bytes, 0); //Converts the byte array to a single integer 
+
+                    /* string message = ReadValue.ToString();
+                    MessageBox.Show("bytes read are {0}", message);*/ //Debug Message
+
+                    if (PanicPercent >= ReadValue)
+                    {
+                        Keys.PressKey('1', true);
+                        Thread.Sleep(5000);
+                        loop --;
+                    }
+                    Thread.Sleep(1000);
+                }
+                
+                
+                //The static address of the pointer (#1) 
+                //int[] iStep2_Offsets = { 0x98, 0x4, 0x288, 0x24, 0x458 }; //Offsets from bottom to top (#2-#6) 
+
+                //int bytesWritten; //Holds how many bytes were written by PointerWrite 
+
+                //int iValue_To_Write = 1000; //Value that we want to write (Step2 requires that you change the value to 1000) 
+                //byte[] bValue_To_Write = BitConverter.GetBytes(iValue_To_Write); //Turns 1000 into bytes 
+
+                //string sWritten_Address = oMemory.PointerWrite((IntPtr)iStep2_Address, //PointerWrite starting with our Step2 Address 
+                 //                 bValue_To_Write, //The value to write (as bytes) 
+                //                  iStep2_Offsets, //Our offsets 
+                 //                 out bytesWritten); //Stores the # of Written Bytes 
+
+                //if (bytesWritten == bValue_To_Write.Length) //If writing was successful 
+                   // MessageBox.Show("Wrote " + iValue_To_Write.ToString() + " to " + sWritten_Address + "!"); //Notify the user of success 
+                //else
+                  //  MessageBox.Show("There was an error writing " + iValue_To_Write.ToString() + " to " + sWritten_Address + "."); //Notify the user of failure 
+
+                oMemory.CloseHandle(); //Close Memory Handle 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+ 
+/* class implementation */ 
+//IntPtr hProc = IntPtr.Zero; 
+//Int32 dwBytesRead, Value; 
+//hProc = OpenProcess(0x0010 /* PROCESS_VM_READ access */, false,iPID); // iPID is your process id get it by your self using Process class 
+//byte[] memory = new byte[6]; // can be more than 6 depends how many bytes you're interesting to read 
+////ReadProcessMemory(hProc,(IntPtr)0xFFFFFFFF /* your address */, out memory, memory.Length,out dwBytesRead); 
+//Value = BitConvert.ToInt32(memory); 
 
 
 
@@ -71,5 +181,5 @@ namespace WindowsFormsApplication1
 
         }
 
-    }
+     }
 
