fac2f603c276c065c35ddf9c3d2457eee31d3d08
malbolger@gmail.com <malbolger@gmail.com@96c2f633-49b2-c093-6fe6-0b779d882d05>
2012-02-17 10:01:49 +0000

    ** Added simple combat bot
    ** Added ability to configure what keys your abilities are set to
    
    ** Added memory addresses for stamina and enemy life.
    
    ** Created methods for Life/Stamina/EnemyLife to be called from bot.

--- Project Files/DAoC Hook/DAoC Hook/Form1.cs
+++ Project Files/DAoC Hook/DAoC Hook/Form1.cs
@@ -15,8 +15,7 @@ using System.Globalization;
 
 namespace WindowsFormsApplication1
 {
-    
-
+  
     public partial class Form1 : Form
     {
         [DllImport("kernel32.dll")]
@@ -35,19 +34,15 @@ namespace WindowsFormsApplication1
 
         Process[] processes = Process.GetProcessesByName("game.dll");  //looks for in running processes.
         Memory oMemory = new Memory(); //For memory read/write
-        Memory value = new Memory();
-
-        // DAoC Life Pointer? 0145CDF8
-
-//               [DllImport("Kernel32.Dll")] 
-//public static extern IntPtr OpenProcess(WindowHandle dwDesiredAccess, bool bInheritance, DWORD dwProcessId); 
-
-//[DllImport("Kernel32.Dll")] 
-//public static extern bool ReadProcessMemory(IntPtr hProcess,IntPtr BaseAddress, out byte[] buffer, Int32 b_size, out Int32 dwBytesRead); 
 
 
 
 
+        public int LifeAddr = Addr.ToDec("0145CDF8");       //4byte
+        public int StamAddr = Addr.ToDec("0145CE00");       //4byte
+        public int EnemyLifeAddr = Addr.ToDec("0145CE0C");  //4byte
+        public int XCoOrdAddr = Addr.ToDec("00980520");     //float
+        public int YcoOrdAddr = Addr.ToDec("00980524");     //float
  
 
         public Form1()
@@ -56,89 +51,159 @@ namespace WindowsFormsApplication1
         }
 
 
+        public int LifeStat()
+        {
+            int lifeBytesRead; //This will hold the number of bytes that were successfully read 
+
+            byte[] LifeBytes = oMemory.Read((IntPtr)LifeAddr, 4, out  lifeBytesRead); //Reads 4 bytes to a byte array 
+            int lifeValue = BitConverter.ToInt32(LifeBytes, 0); //Converts the byte array to a single integer
+                       
+            return lifeValue;
+        }
+
+        public int StamStat()
+        {
+            int stamBytesRead;
+
+            byte[] StamBytes = oMemory.Read((IntPtr)StamAddr, 4, out stamBytesRead);
+            int stamValue = BitConverter.ToInt32(StamBytes, 0);
+
+            return stamValue;
+        }
+
+        public int EnemyLife()
+        {
+            int enemyLifeBytesRead;
+
+            byte[] EnemyLifeBytes = oMemory.Read((IntPtr)EnemyLifeAddr, 4, out enemyLifeBytesRead);
+            int enemyLifeValue = BitConverter.ToInt32(EnemyLifeBytes, 0);
+
+            return enemyLifeValue;
+
+        }
+
+        bool combatloop = false;
+        bool resting = false;
 
         private void button1_Click(object sender, EventArgs e)
         {
 
 
-            if (processes.Length == 0) 
-                throw new Exception("Please make sure DAoC is running and retry."); //exception if cannot find process.
-
-            
+            if (processes.Length != 0)
+            {
 
 
-            IntPtr WindowHandle = processes[0].MainWindowHandle;
-            WindowsAPI.SwitchWindow(WindowHandle);
-            Thread.Sleep(1000); //wait while window is switched
-            //WindowsAPI.SetFocus(WindowHandle);  //sets processes window to focused window to send commands.
-            while (true)
-            Keys.PressKey('1' , true);
+                string text = textBox1.Text;
+                char input;
+                input = text[0];
+                IntPtr WindowHandle = processes[0].MainWindowHandle;
+                WindowsAPI.SwitchWindow(WindowHandle);
+                Thread.Sleep(1000); //wait while window is switched
 
+
+                if (combatloop == true)
+                    combatloop = false;
+                else
+                {
+                    combatloop = true;
+                    do
+                    {
+                        oMemory.ReadProcess = processes[0]; //Sets the Process to Read/Write From/To 
+                        oMemory.Open(); //Open Process 
+
+                        int stamcheck = StamStat();
+                        int lifecheck = LifeStat();
+                        int enemyLife = EnemyLife();
+
+
+                        //string message = enemyLife.ToString();
+                        //string lifemessage = life.ToString();
+                        //MessageBox.Show(message, lifemessage); //Debug Message
+
+                        if (enemyLife > 0 && enemyLife <= 100)
+                        {
+                            Keys.PressKey(input, true);
+                            Thread.Sleep(2500); //Wait between sends
+                        }
+                        else if(lifecheck < 100 || stamcheck < 100)
+                        {
+                            do
+                            {
+                                int stam = StamStat();
+                                int life = LifeStat();
+
+                                if (life < 100 || stam < 100 && resting == false)
+                                {
+                                    Keys.PressKey('x', true); //sit
+                                    resting = true;
+                                    Thread.Sleep(5000);
+
+                                }
+                                else if (life == 100 && stam == 100)
+                                {
+                                    Keys.PressKey('x', true); //standxx
+                                    resting = false;
+                                    Thread.Sleep(5000);
+                                }
+                                else
+                                {
+                                    Thread.Sleep(5000);
+                                }
+                            }
+                            while (resting == true);
+                        }
+                    }
+                    while (combatloop == true);
+                }
+            }
         }
 
+        bool healthcheckloop = false;
 
         private void button2_Click(object sender, EventArgs e)
         {
+            
             if (processes.Length != 0) //If the process exists 
             {
                 IntPtr WindowHandle = processes[0].MainWindowHandle;
                 WindowsAPI.SwitchWindow(WindowHandle);
                 Thread.Sleep(1000); //wait while window is switched
-              
-                int LifeAddr = Addr.ToDec("0145CDF8");
-                oMemory.ReadProcess = processes[0]; //Sets the Process to Read/Write From/To 
-                oMemory.Open(); //Open Process 
 
 
-
-               
-                
-
-          
-               // int Life = bytesRead;
-                int PanicPercent = 20;
-
-                int loop = 15;
-
-              
-
-                while (loop > 1)
+                if (healthcheckloop == true)
+                    healthcheckloop = false;
+                else
                 {
-                    int bytesReaded; //This will hold the number of bytes that were successfully read 
-                    byte[] ThisIs4Bytes = oMemory.Read((IntPtr)LifeAddr, 4, out  bytesReaded); //Reads 4 bytes to a byte array 
-                    int ReadValue = BitConverter.ToInt32(ThisIs4Bytes, 0); //Converts the byte array to a single integer 
+                    healthcheckloop = true;
+                 do
+                {           
+                        oMemory.ReadProcess = processes[0]; //Sets the Process to Read/Write From/To 
+                        oMemory.Open(); //Open Process 
 
-                    /* string message = ReadValue.ToString();
-                    MessageBox.Show("bytes read are {0}", message);*/ //Debug Message
 
-                    if (PanicPercent >= ReadValue)
-                    {
-                        Keys.PressKey('1', true);
-                        Thread.Sleep(5000);
-                        loop --;
+                        int panicPercent = 20;
+                        int enemyLife = EnemyLife();
+                        int life = LifeStat();
+                        int stam = StamStat();
+
+
+
+
+                        /* string message = life.ToString();
+                         MessageBox.Show("bytes read are {0}", message); */
+                        //Debug Message 
+
+                        if (panicPercent >= life)
+                        {
+                            Keys.PressKey('1', true);
+                            Thread.Sleep(2000);
+                        }
+                        Thread.Sleep(1000);
                     }
-                    Thread.Sleep(1000);
+
+                 while (healthcheckloop == true);
+
                 }
-                
-                
-                //The static address of the pointer (#1) 
-                //int[] iStep2_Offsets = { 0x98, 0x4, 0x288, 0x24, 0x458 }; //Offsets from bottom to top (#2-#6) 
-
-                //int bytesWritten; //Holds how many bytes were written by PointerWrite 
-
-                //int iValue_To_Write = 1000; //Value that we want to write (Step2 requires that you change the value to 1000) 
-                //byte[] bValue_To_Write = BitConverter.GetBytes(iValue_To_Write); //Turns 1000 into bytes 
-
-                //string sWritten_Address = oMemory.PointerWrite((IntPtr)iStep2_Address, //PointerWrite starting with our Step2 Address 
-                 //                 bValue_To_Write, //The value to write (as bytes) 
-                //                  iStep2_Offsets, //Our offsets 
-                 //                 out bytesWritten); //Stores the # of Written Bytes 
-
-                //if (bytesWritten == bValue_To_Write.Length) //If writing was successful 
-                   // MessageBox.Show("Wrote " + iValue_To_Write.ToString() + " to " + sWritten_Address + "!"); //Notify the user of success 
-                //else
-                  //  MessageBox.Show("There was an error writing " + iValue_To_Write.ToString() + " to " + sWritten_Address + "."); //Notify the user of failure 
-
                 oMemory.CloseHandle(); //Close Memory Handle 
             }
         }
@@ -148,38 +213,12 @@ namespace WindowsFormsApplication1
 
         }
 
-      
- 
-/* class implementation */ 
-//IntPtr hProc = IntPtr.Zero; 
-//Int32 dwBytesRead, Value; 
-//hProc = OpenProcess(0x0010 /* PROCESS_VM_READ access */, false,iPID); // iPID is your process id get it by your self using Process class 
-//byte[] memory = new byte[6]; // can be more than 6 depends how many bytes you're interesting to read 
-////ReadProcessMemory(hProc,(IntPtr)0xFFFFFFFF /* your address */, out memory, memory.Length,out dwBytesRead); 
-//Value = BitConvert.ToInt32(memory); 
-
-
-
-
-     /*   public static void SendKeyAsInput(System.Windows.Forms.Keys key, int HoldTime)      //I am hoping that my SendKeyAsInput(Keys.J , 500); is utilizing this correctly.  
+        private void Form1_Load(object sender, EventArgs e)
         {
-            INPUT INPUT1 = new INPUT();
-            INPUT1.type = (int)InputType.INPUT_KEYBOARD;
-            INPUT1.ki.wVk = (short)key;
-            INPUT1.ki.dwFlags = (int)KEYEVENTF.KEYDOWN;
-            INPUT1.ki.dwExtraInfo = GetMessageExtraInfo();
-            SendInput(1, new INPUT[] { INPUT1 }, Marshal.SizeOf(INPUT1));
-
-            WaitForSingleObject((IntPtr)0xACEFDB, (uint)HoldTime);
-
-            INPUT INPUT2 = new INPUT();
-            INPUT2.type = (int)InputType.INPUT_KEYBOARD;
-            INPUT2.ki.wVk = (short)key;
-            INPUT2.ki.dwFlags = (int)KEYEVENTF.KEYUP;
-            INPUT2.ki.dwExtraInfo = GetMessageExtraInfo();
-            SendInput(1, new INPUT[] { INPUT2 }, Marshal.SizeOf(INPUT2)); */
-
         }
 
-     }
+
+    }
+
+}
 
